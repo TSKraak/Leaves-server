604dcc777d73a9ff2b3d07fc184ed6375f60ae85
TSKraak <tskraak@gmail.com>
2020-12-01 17:20:10 +0100

    Changed database url

--- config/config.js
+++ config/config.js
@@ -14,11 +14,7 @@ module.exports = {
     dialect: "postgres",
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
     dialect: "postgres",
-    use_env_variable: process.env.ELEPHANT_SQL,
+    use_env_variable: "DATABASE_URL",
   },
 };
