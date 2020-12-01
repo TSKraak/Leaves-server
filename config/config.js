be345f33aac2f3ed6cbdf8a5afd58c4fcba71de2
TSKraak <tskraak@gmail.com>
2020-12-01 16:57:05 +0100

    Heroku deployment settings added

--- config/config.js
+++ config/config.js
@@ -11,13 +11,14 @@ module.exports = {
     password: null,
     database: "database_test",
     host: "127.0.0.1",
-    dialect: "mysql",
+    dialect: "postgres",
   },
   production: {
     username: "root",
     password: null,
     database: "database_production",
     host: "127.0.0.1",
-    dialect: "mysql",
+    dialect: "postgres",
+    use_env_variable: process.env.ELEPHANT_SQL,
   },
 };
