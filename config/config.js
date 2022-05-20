77bb0b9a55d52dc422f35cd5a97d2248c60bc8d8
TSKraak <tskraak@gmail.com>
2022-05-20 15:38:26 +0200

    Added dialect options to sequelize config

--- config/config.js
+++ config/config.js
@@ -16,5 +16,10 @@ module.exports = {
   production: {
     dialect: "postgres",
     use_env_variable: "DATABASE_URL",
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
